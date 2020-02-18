fcf5d8f2f8a8a6bbdf35e9baacf8a3a2c738d038
yyavor0705 <y0507yavor@gmail.com>
2020-02-18 16:46:30 +0200

    Implementation of https://github.com/yyavor0705/TimeShedule/issues/2 (Add js table creation)

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import * as $ from "jquery";
-import { Table, Worker, Job } from "./tabel"
+import { MarkupTable, Worker, Job, SchedulesHolderTable } from "./tabel"
 import fakeData = require('./fake_data/fake-data.json')
 
 const myObj: {[index: string]:any} = fakeData
@@ -7,27 +7,25 @@ const myObj: {[index: string]:any} = fakeData
 let sheetContainer = null;
 
 $( document ).ready(function() {
-    let mainContainer = $("#main-container");
-    // sheetContainer = $("#schedule-sheet-container");
-    // if (sheetContainer.length){
-    //     let workers:Worker[] = [];
-    //     let workerRowCounter = 1 // initial number has to be 1 as first row belongs to header
+    sheetContainer = $("#schedule-sheet-container");
+    if (sheetContainer.length){
+        let workers:Worker[] = [];
 
-    //     for (let key in fakeData) {
+        for (let key in fakeData) {
+            let newWorker = new Worker()
+            newWorker.id = key
+            newWorker.name = myObj[key].name
+            for (let job in myObj[key].jobs) {
+                let jobObj = myObj[key].jobs[job]
+                let newJob = new Job(job, jobObj.start_time, jobObj.duration, jobObj.color)
+                newWorker.addJob(newJob)
+            }
+            workers.push(newWorker)
             
-    //         workerRowCounter += 1
-    //         let newWorker = new Worker()
-    //         newWorker.id = key
-    //         newWorker.name = myObj[key].name
-    //         for (let job in myObj[key].jobs) {
-    //             let jobObj = myObj[key].jobs[job]
-    //             let newJob = new Job(job, jobObj.start_time, jobObj.duration, jobObj.color)
-    //             newWorker.jobs.push(newJob)
-    //         }
-    //         workers.push(newWorker)
-            
-    //     }
-    //     let table = new Table(workers);
-    //     mainContainer.append(table.get_element());
-    // }
+        }
+        let markupTable = new MarkupTable(workers.length);
+        sheetContainer.append(markupTable.get_element())
+        let schedulesHolderTable = new SchedulesHolderTable(workers)
+        sheetContainer.append(schedulesHolderTable.get_element())
+    }
 });
--- tabel.ts
+++ tabel.ts
@@ -21,11 +21,10 @@ export class Job implements JqueryElm{
         }
 
     private _convert_start_time_to_position(startTime:string): number {
-        let position_correction = 1
         let splitted = startTime.split(":", 2);
         let hours = parseInt(splitted[0]);
         let minutes = parseInt(splitted[1]);
-        let position = Math.floor(hours*60)+minutes+position_correction;
+        let position = Math.floor(hours*60)+minutes;
         return position
     }
 
@@ -34,42 +33,77 @@ export class Job implements JqueryElm{
     }
 }
 
-export class Worker {
+export class Worker implements JqueryElm{
     id: string
     name: string
     jobs:Job[] = []
+    private trElement = $("<tr>")
+    private tdElement = $("<td class='one'>")
+
+    constructor (){
+        this.trElement.append(this.tdElement)
+    }
+
+    addJob(job: Job){
+        this.jobs.push(job)
+        this.tdElement.append(job.get_element())
+    }
+
+    get_element(){
+        return this.trElement
+    }
 }
 
-export class Table implements JqueryElm{
-    tableElm = $("<table>")
+export class MarkupTable implements JqueryElm{
+    tableElm = $("<table id='markup'>")
     tableHeaderRow = $('<tr id="table-header">');
     totalWorkingHours = 24;
 
-    constructor(workers: Worker[]) {
-        this.tableElm.append(this.tableHeaderRow)
+    constructor(workersQuantity: Number) {
+        this.tableElm.append(this.tableHeaderRow);
         this.create_header();
-        this.create_workers_rows(workers);
+        this.create_workers_rows(workersQuantity);
     }
 
     private create_header() {
         let tdElementsCounter = 0;
-        while (tdElementsCounter <= this.totalWorkingHours) {
+        while (tdElementsCounter < this.totalWorkingHours) {
             this.tableHeaderRow.append($("<td>"+tdElementsCounter+":00</td>"))
             tdElementsCounter += 1
         }
     }
 
-    private create_workers_rows(workers: Worker[]) {
-        for (let worker in workers) {
+    private create_workers_rows(workersQuantity: Number) {
+        let workerCounter = 1
+        while (workerCounter <= workersQuantity) {
             let trElm = $('<tr class="schedule-row">')
             this.tableElm.append(trElm)
 
             let columnNumber = 0
-            while (columnNumber <= this.totalWorkingHours) {
+            while (columnNumber < this.totalWorkingHours) {
                 trElm.append("<td>")
                 columnNumber += 1
             }
+            workerCounter += 1
             
+        }
+    }
+
+    get_element() {
+        return this.tableElm
+    }
+}
+
+export class SchedulesHolderTable implements JqueryElm {
+    tableElm = $("<table id='schedules-holder'>")
+
+    constructor(private workersArray: Worker[]) {
+        this.addWorkerRows()
+    }
+
+    private addWorkerRows() {
+        for (let worker in this.workersArray) {
+            this.tableElm.append(this.workersArray[worker].get_element())            
         }
     }
 
